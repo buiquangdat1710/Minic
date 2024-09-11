508e21fa30ee982ead0baa77223eb0dc553f954c
BuiQuangDat <80840330+buiquangdat1710@users.noreply.github.com>
2024-09-11 23:33:07 +0700

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,95 @@
+import streamlit as st
+from openai import OpenAI
+from dotenv import load_dotenv
+import time
+import os
+import streamlit.components.v1 as com
+
+page_bg_img = '''
+<style>
+.stApp {
+  background-image: url("https://images.pexels.com/photos/18696053/pexels-photo-18696053/free-photo-of-thien-nhien-t-i-m-u-k-t-c-u.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2");
+  background-size: cover;
+}
+</style>
+'''
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+# Login section
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+
+if not st.session_state.logged_in:
+    st.title("Login to Minic")
+    with st.form(key='login_form'):
+        username = st.text_input("Username:", key="username", help="Enter your username", placeholder="Your username")
+        password = st.text_input("Key Code:", type="password", key="password", help="Enter your key code", placeholder="Key Code")
+        submit_button = st.form_submit_button("Log in", help="Click to Log in")
+
+        if submit_button:
+            load_dotenv()
+            key_code = os.getenv("KEY_CODE")
+            if password == key_code:
+                st.session_state.logged_in = True
+                st.session_state.name = username
+                st.success("Log in successfully!")
+            else:
+                st.error("Wrong key, please [contact me](https://www.facebook.com/buiquangdat2004?locale=vi_VN) to receive the key code.")
+else:
+    # Phần còn lại của ứng dụng sau khi đăng nhập thành công
+    st.markdown(
+        """
+        <div style="text-align: center;">
+            <h2>Minic - The Simple Chatbot</h2>
+        </div>
+        """, 
+        unsafe_allow_html=True
+    )
+    load_dotenv()
+    api_key = os.getenv("OPENAI_API_KEY")
+    client = OpenAI(api_key = api_key)
+
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+    if "messages" not in  st.session_state:
+        st.session_state.messages = []
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if prompt := st.chat_input("Hãy nhập vào yêu cầu?"):
+        st.session_state.messages.append(
+            {
+                "role": "user",
+                "content": prompt
+            }
+        )
+
+        with st.chat_message('user'):
+            st.markdown(prompt)
+
+        with st.chat_message('assistant'):
+            full_res = ""
+            holder = st.empty()
+
+            for response in client.chat.completions.create(
+                model = st.session_state["openai_model"],
+                messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ],
+                stream=True,
+            ):
+                full_res += (response.choices[0].delta.content or "")
+                holder.markdown("Minic: " + full_res + "▌")
+                holder.markdown("Minic: " + full_res)
+            holder.markdown("Minic: " + full_res)
+
+        st.session_state.messages.append(
+            {
+                "role": "assistant",
+                "content": full_res
+            }
+        )
